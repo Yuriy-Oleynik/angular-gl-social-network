b9006d27f36d5d16858a32b4e90f2584b3cdc7da
Yurii Oliinyk <yurikua11@gmail.com>
2020-12-15 18:50:05 +0200

    Added list communities component

---
+++ src/app/community/list-communities/list-communities.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListCommunitiesComponent } from './list-communities.component';
+
+describe('ListCommunitiesComponent', () => {
+  let component: ListCommunitiesComponent;
+  let fixture: ComponentFixture<ListCommunitiesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListCommunitiesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListCommunitiesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
