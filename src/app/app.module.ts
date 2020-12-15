ebbb69118e5c7e0e59b257cc1c0ee96eb1c4df0a
Yurii Oliinyk <yurikua11@gmail.com>
2020-12-15 19:45:31 +0200

    Fixed issues, updated images and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,68 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { TokenInterceptor } from './token-interceptor';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { CommunitySideBarComponent } from './shared/community-side-bar/community-side-bar.component';
+import { CreateCommunityComponent } from './community/create-community/create-community.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ListCommunitiesComponent } from './community/list-communities/list-communities.component';
+import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { ViewPostComponent } from './post/view-post/view-post.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { ImageUploadComponent } from './shared/image-upload/image-upload.component';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    PostTileComponent,
+    VoteButtonComponent,
+    SideBarComponent,
+    CommunitySideBarComponent,
+    CreateCommunityComponent,
+    CreatePostComponent,
+    ListCommunitiesComponent,
+    ViewPostComponent,
+    UserProfileComponent,
+    ImageUploadComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        NgxWebstorageModule.forRoot(),
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        FontAwesomeModule,
+        EditorModule,
+        NgbModule,
+        FroalaEditorModule.forRoot(),
+        FroalaViewModule.forRoot(),
+        FormsModule
+    ],
+  providers: [
+    { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
